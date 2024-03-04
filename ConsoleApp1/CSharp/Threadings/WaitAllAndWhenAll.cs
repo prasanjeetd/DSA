4af5b38876bfec2a832cab9f8ae6dda38804ecc8
Prasanjeet Debnath <pjeet_4411@hotmail.com>
2024-03-04 15:45:29 +0530

    asyn await for concurrency

--- ConsoleApp1/CSharp/Threadings/WaitAllAndWhenAll.cs
+++ ConsoleApp1/CSharp/Threadings/WaitAllAndWhenAll.cs
@@ -44,11 +44,14 @@ namespace ConsoleApp1.CSharp.Threadings
             Console.WriteLine("Completed Task by wait all");
 
             var t3 = Diplay2();
-            var t4 = Diplay3();
+            var t4 = Diplay4();
 
             Console.WriteLine("Waiting Task by when all");
 
             Task.WaitAll(t3, t4);
+
+            Console.WriteLine($"Task 4 compeleted {t4.Result}");
+
 
             Console.WriteLine("Completed Task by when all");
         }
@@ -85,7 +88,7 @@ namespace ConsoleApp1.CSharp.Threadings
         {
             Console.WriteLine("Thread {0} - Start ", Thread.CurrentThread.ManagedThreadId);
 
-            Task.Delay(5000);
+            Thread.Sleep(5000);
 
             Console.WriteLine("Thread {0} - End ", Thread.CurrentThread.ManagedThreadId);
 
@@ -96,7 +99,7 @@ namespace ConsoleApp1.CSharp.Threadings
         {
             Console.WriteLine("Thread {0} - Start ", Thread.CurrentThread.ManagedThreadId);
 
-            Task.Delay(5000);
+            Thread.Sleep(5000);
 
             Console.WriteLine("Thread {0} - End ", Thread.CurrentThread.ManagedThreadId);
 
@@ -125,6 +128,17 @@ namespace ConsoleApp1.CSharp.Threadings
             Console.WriteLine("Thread {0} - End ", Thread.CurrentThread.ManagedThreadId);
 
             return "World2";
+        }
+
+        public async Task<string> Diplay4()
+        {
+            Console.WriteLine("Thread {0} - Start ", Thread.CurrentThread.ManagedThreadId);
+
+            Thread.Sleep(5000);
+
+            return await Task.Run(() => "World2");
+
+
         }
     }
 }
